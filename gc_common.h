3847cabd811a73cf24971515dd8873a37d9e82e1
GameChaos <franzkits@gmail.com>
2023-07-06 01:17:58 +0300

    Successfully sigscan and call a function

---
+++ gc_common.h
@@ -0,0 +1,79 @@
+/* date = October 12th 2020 6:33 pm */
+
+#include <stdint.h>
+#include <float.h>
+#include <limits.h>
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#define internal static
+#define local_persist static
+#define global static
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+typedef s32 b32;
+typedef s16 b16;
+typedef s8 b8;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef float f32;
+typedef double f64;
+
+#define S8_MIN (-128)
+#define S16_MIN (-32768)
+#define S32_MIN (-2147483648LL)
+#define S64_MIN (-9223372036854775808LL)
+
+#define U8_MAX 0xffU
+#define S8_MAX 0x7f
+#define U16_MAX 0xffffU
+#define S16_MAX 0x7fff
+#define U32_MAX 0xffffffffU
+#define S32_MAX 0x7fffffff
+#define U64_MAX 0xffffffffffffffffULL
+#define S64_MAX 0x7fffffffffffffffLL
+
+#define F32_MAX FLT_MAX
+#define F64_MAX DBL_MAX
+
+#define KILOBYTES(value) ((value) * 1024)
+#define MEGABYTES(value) (KILOBYTES(value) * 1024ULL)
+#define GIGABYTES(value) (MEGABYTES(value) * 1024ULL)
+#define TERABYTES(value) (GIGABYTES(value) * 1024ULL)
+
+#define THOUSAND(a) ((a) * 1000)
+#define MILLION(a) (THOUSAND(a) * 1000)
+#define BILLION(a) (MILLION(a) * 1000ULL)
+
+#define ARRAYCOUNT(array) (sizeof(array) / sizeof((array)[0]))
+
+#ifdef GC_DEBUG
+#define ASSERT(expression) do {if (!(expression)) (*(volatile int *)0) = 0; } while(0)
+#else
+#define ASSERT(expression)
+#endif
+
+#define MIN(a, b) ((a) > (b) ? (b) : (a))
+#define MAX(a, b) ((a) < (b) ? (b) : (a))
+#define ABS(a) ((a) < 0 ? -(a) : (a))
+#define CLAMP(min, val, max) (MAX(MIN(val, max), min))
+#define ROUND_UP_BY_POWER_OF_2(value, powerOf2) (((value) + (powerOf2) - 1) & ~((powerOf2) - 1))
+#define ROUND_DOWN_BY_POWER_OF_2(value, powerOf2) ((value) & ~((powerOf2) - 1))
+
+#define MERGE_(a,b)  a##b
+#define MERGE(a,b)  MERGE_(a, b)
+#define LABEL_(a) MERGE_(unique_name_, a)
+#define UNIQUE_NAME LABEL_(__LINE__)
+#define STRING(s) #s
+
+#define MEMBER_SIZE(type, member) sizeof(((type *)0)->member)
+
+#endif //COMMON_H
--- stub_mm.cpp
+++ stub_mm.cpp
@@ -18,8 +18,18 @@
 #include <stdio.h>
 #include "stub_mm.h"
 
+#include "gc_common.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#include <psapi.h>
+#else
+#error Only windows supported.
+#endif
+
 SH_DECL_HOOK3_void(ISource2Server, GameFrame, SH_NOATTRIB, false, bool, bool, bool);
 SH_DECL_HOOK1_void(ISource2GameClients, ClientFullyConnect, SH_NOATTRIB, false, CPlayerSlot);
+SH_DECL_HOOK5(ISource2GameClients, ProcessUsercmds, SH_NOATTRIB, false, float, CPlayerSlot, bf_read *, int, bool, bool);
 
 StubPlugin g_StubPlugin;
 ISource2GameClients *gameclients = NULL;
@@ -30,6 +40,12 @@ CGlobalVars* gpGlobals = NULL;
 
 void Hook_GameFrame(bool simulating, bool bFirstTick, bool bLastTick);
 void Hook_ClientFullyConnect(CPlayerSlot slot);
+float Hook_ProcessUsercmds(CPlayerSlot slot, bf_read *buf, int numcmds, bool ignore, bool paused);
+
+uintptr_t FindPattern(void *start, size_t maxScanBytes, char *pattern, char *ignorePattern);
+
+typedef void *PlayerSlotToPlayerController_t(CPlayerSlot slot);
+PlayerSlotToPlayerController_t *PlayerSlotToPlayerController = NULL;
 
 PLUGIN_EXPOSE(StubPlugin, g_StubPlugin);
 bool StubPlugin::Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, bool late)
@@ -41,11 +57,36 @@ bool StubPlugin::Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, bo
 	GET_V_IFACE_CURRENT(GetServerFactory, serverconfig, ISource2ServerConfig, INTERFACEVERSION_SERVERCONFIG);
 	GET_V_IFACE_CURRENT(GetServerFactory, gameclients, ISource2GameClients, INTERFACEVERSION_SERVERGAMECLIENTS);
 	
-	META_CONPRINTF("BSADFJBSADJFBK %s\n\n\n\n\n\n", "poopoo");
-	META_CONPRINTF("game dll %p\n\n\n\n\n\n", gamedll);
-	
 	SH_ADD_HOOK(ISource2Server, GameFrame, gamedll, &Hook_GameFrame, false);
 	SH_ADD_HOOK(ISource2GameClients, ClientFullyConnect, gameclients, SH_STATIC(Hook_ClientFullyConnect), false);
+	SH_ADD_HOOK(ISource2GameClients, ProcessUsercmds, gameclients, SH_STATIC(Hook_ProcessUsercmds), false);
+	
+	// windows
+	char *signature = "\x40\x53\x48\x83\xEC\x20\x48\x8B\x05\x27\x27\x27\x27\x48\x85\xC0\x74\x3D";
+	char *mask = "xxxxxxxxx....xxxxx";
+	
+#ifdef _WIN32
+	char *binPath = "../../csgo/bin/win64/server.dll";
+	HMODULE bin = LoadLibrary(binPath);
+	
+	MODULEINFO info = {0};
+	GetModuleInformation(GetCurrentProcess(), bin, &info, sizeof(info));
+	
+	s64 binBytes = info.SizeOfImage;
+#elif __linux__
+	// untested
+	char *binPath = "server.so";
+	auto *bin = dlopen(binPath, RTLD_NOW);
+	
+	s64 binBytes = MEGABYTES(50);
+#endif
+	
+	if (!bin)
+	{
+		snprintf(error, maxlen, "Could not open %s", binPath);
+	}
+	
+	PlayerSlotToPlayerController = (PlayerSlotToPlayerController_t *)SigScan(bin, binBytes, signature, mask);
 	
 	return true;
 }
@@ -54,6 +95,7 @@ bool StubPlugin::Unload(char *error, size_t maxlen)
 {
 	SH_REMOVE_HOOK(ISource2Server, GameFrame, gamedll, &Hook_GameFrame, false);
 	SH_REMOVE_HOOK(ISource2GameClients, ClientFullyConnect, gameclients, SH_STATIC(Hook_ClientFullyConnect), false);
+	SH_REMOVE_HOOK(ISource2GameClients, ProcessUsercmds, gameclients, SH_STATIC(Hook_ProcessUsercmds), false);
 	
 	return true;
 }
@@ -61,12 +103,11 @@ bool StubPlugin::Unload(char *error, size_t maxlen)
 void Hook_GameFrame(bool simulating, bool bFirstTick, bool bLastTick)
 {
 	gpGlobals = engine->GetServerGlobals();
-	// META_CONPRINTF("Hook_GameFrame gpGlobals %p\n\n\n\n\n\n", gpGlobals);
-	// engine->ClientPrintf(CPlayerSlot(0), "peepoo");
 }
 
 void Hook_ClientFullyConnect(CPlayerSlot slot)
 {
+	void *test = PlayerSlotToPlayerController(slot);
 	engine->ClientCommand(slot, "say poopee");
 	gpGlobals = engine->GetServerGlobals();
 	META_CONPRINTF("player slot: %i\n", slot.Get());
@@ -88,6 +129,36 @@ void Hook_ClientFullyConnect(CPlayerSlot slot)
 	META_CONPRINTF("tickcount: %i\n", gpGlobals->tickcount);
 	META_CONPRINTF("interval_per_tick: %f\n", gpGlobals->interval_per_tick);
 	META_CONPRINTF("mapname: %s\n", gpGlobals->mapname);
+}
+
+float Hook_ProcessUsercmds(CPlayerSlot slot, bf_read *buf, int numcmds, bool ignore, bool paused)
+{
+	
+	RETURN_META_VALUE(MRES_IGNORED, 0.0f);
+}
+
+uintptr_t SigScan(void *start, size_t maxScanBytes, char *pattern, char *ignorePattern)
+{
+	uintptr_t patternLen = strlen(ignorePattern);
+	
+	char *memory = (char *)start;
+	for (uintptr_t i = 0; i < maxScanBytes; i++)
+	{
+		uintptr_t matches = 0;
+		while (memory[i + matches] == pattern[matches] || ignorePattern[matches] != 'x')
+		{
+			matches++;
+			if (matches == 14)
+			{
+				matches = matches;
+			}
+			if (matches == patternLen)
+			{
+				return (uintptr_t)(memory + i);
+			}
+		}
+	}
+	return NULL;
 }
 
 void StubPlugin::AllPluginsLoaded()
